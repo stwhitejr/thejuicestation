951ea797ed6ad58e43f05f764281b311d5bdf310
Steve <stwhitejr@gmail.com>
2017-11-25 16:35:00 -0500

    updating header

--- www/views/base_view.php
+++ www/views/base_view.php
@@ -10,26 +10,28 @@
     <link href="includes/css/home.css" type="text/css" rel="stylesheet" />
   </head>
   <body>
-    <header class="Header u-FlexBox">
-      <div class="Logo">
-        <img src="includes/images/logo.svg" alt="The Juice Station" class="Logo-img" />
-      </div>
-      <nav class="Nav">
-        <div class="Nav-inner">
-          <?php
-          foreach ($nav_items as $nav) {
-          ?>
-            <a href="/<?=$nav['url']?>" class="Nav-item">
-              <?=$nav['text']?>
-            </a>
-          <?php
-          }
-          ?>
+    <div class="Wrap">
+      <header class="Header u-FlexBox">
+        <div class="Logo">
+          <img src="includes/images/logo.svg" alt="The Juice Station" class="Logo-img" />
         </div>
-      </nav>
-    </header>
-    <?php $view->output() ?>
-    <footer>
-    </footer>
+        <nav class="Nav">
+          <div class="Nav-inner">
+            <?php
+            foreach ($nav_items as $nav) {
+            ?>
+              <a href="/<?=$nav['url']?>" class="Nav-item">
+                <?=$nav['text']?>
+              </a>
+            <?php
+            }
+            ?>
+          </div>
+        </nav>
+      </header>
+      <?php require_once($content_view) ?>
+      <footer>
+      </footer>
+    </div>
   </body>
 </html>
