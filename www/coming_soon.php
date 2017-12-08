2dbd33d18b162d7fcec2e78ad7ca9189908f89d6
Steve White <stwhite@wayfair.com>
2017-12-08 10:19:38 -0500

    social media icons

--- www/coming_soon.php
+++ www/coming_soon.php
@@ -14,7 +14,7 @@
         <h1>Website and New Pembroke Location Coming Soon!</h1>
         <p class="Intro">
           We're happy to announce The Juice Station will be opening it's first location at
-          <emphasis>801&nbsp;Washington&nbsp;St, Pembroke,&nbsp;MA</emphasis> in January 2018!
+          <emphasis>808&nbsp;Washington&nbsp;St, Pembroke,&nbsp;MA</emphasis> in January 2018!
         </p>
         <p class="Intro">
           We're hard at work getting the website and new location up and running but here's a sneak peak at just some of the services we'll have to offer. Also, don't forgot to sign up below for updates on our opening date!
@@ -23,23 +23,13 @@
       <section>
         <div class="flex">
           <div class="flex_col">
-            <ul>
-              <li>Fresh Juices</li>
-              <li>Fresh Juices</li>
-              <li>Fresh Juices</li>
-              <li>Fresh Juices</li>
-              <li>Fresh Juices</li>
-            </ul>
-          </div>
-          <div class="flex_col">
-            <ul>
-              <li>Fresh Juices</li>
-              <li>Fresh Juices</li>
-              <li>Fresh Juices</li>
-              <li>Fresh Juices</li>
-              <li>Fresh Juices</li>
-            </ul>
-          </div>
+          <ul>
+            <li>Fresh Juice</li>
+            <li>Smoothies</li>
+            <li>Cleanses</li>
+            <li>Shots</li>
+            <li>Subscription Delivery Service</li>
+          </ul>
         </div>
       </section>
       <section>
